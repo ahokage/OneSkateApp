f0c2bafb8dd00d7e1523a5b71a98e88c95dc7f2f
Ledion2015 <shahajledjon@gmail.com>
2023-03-29 15:35:55 +0200

    Small changes

--- Dtos/RacerDto.cs
+++ Dtos/RacerDto.cs
@@ -8,9 +8,9 @@ namespace OneSkate.Dtos
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
-        public ClubDto? Club { get; set; }
+        public ClubDto Club { get; set; }
         public int? ClubId { get; set; }
-        public RaceDto? Race { get; set; }
+        public RaceDto Race { get; set; }
         public int? RaceId { get; set; }
     }
 }
--- Models/Racer.cs
+++ Models/Racer.cs
@@ -8,9 +8,9 @@ namespace OneSkate.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
-        public Club? Club { get; set; }
+        public Club Club { get; set; }
         public int? ClubId { get; set; }
-        public Race? Race { get; set; }
+        public Race Race { get; set; }
         public int? RaceId { get; set; }
 
     }
